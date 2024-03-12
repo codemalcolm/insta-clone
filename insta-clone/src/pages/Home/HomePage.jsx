b5ff6657e3453df65203fa97d196cd82476cbe5d
Vojtěch Cvrček <vojta.cvrcek1@gmail.com>
2024-03-12 15:33:52 +0100

    Feed post footer

--- insta-clone/src/pages/Home/HomePage.jsx
+++ insta-clone/src/pages/Home/HomePage.jsx
@@ -1,5 +1,20 @@
+import { Container,Flex, Box } from "@chakra-ui/react";
+import FeedPosts from "../Feed/FeedPosts";
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<Container maxWidth={"container.lg"}>
+			<Flex gap={20}>
+				<Box flex={2} py={10}>
+					<FeedPosts />
+				</Box>
+				<Box flex={3} mr={20}
+				display={{base:"none", lg:"block"}}
+				maxWidth={"300px"} border={"1px solid"}>
+					Suggested Users
+				</Box>
+			</Flex>
+		</Container>
+	);
 };
 
 export default HomePage;
