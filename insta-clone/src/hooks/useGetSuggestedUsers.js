3a907f99b3b4ccbe989dfecf38fc02352791f6f6
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-03-26 20:25:01 +0100

    suggested users sidebar

---
+++ insta-clone/src/hooks/useGetSuggestedUsers.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import useAuthStore from "../store/authStore";
+import useShowToast from "./useShowToast";
+import {
+	collection,
+	getDoc,
+	getDocs,
+	limit,
+	orderBy,
+	query,
+	where,
+} from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
+
+const useGetSuggestedUsers = () => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [suggestedUsers, setSuggestedUsers] = useState(true);
+	const authUser = useAuthStore((state) => state.user);
+	const showToast = useShowToast();
+
+	useEffect(() => {
+		const getSuggestedUsers = async () => {
+			isLoading(true);
+			try {
+				const usersRef = collection(firestore, "users");
+				const q = query(
+					usersRef,
+					where("uid", "not-in", [authUser.uid, ...authUser.following]),
+					orderBy("uid"),
+					limit(3)
+				);
+
+				const querySnapshot = await getDocs(q);
+				const users = [];
+				querySnapshot.forEach((doc) => {
+					users.push({ ...doc.data(), id: doc.id });
+				});
+
+				setSuggestedUsers(users);
+			} catch (error) {
+				showToast("Error", error.message, "error");
+			} finally {
+				isLoading(false);
+			}
+		};
+		if (authUser) getSuggestedUsers;
+	}, [authUser, showToast]);
+    return { isLoading, suggestedUsers}
+};
+
+export default useGetSuggestedUsers;
--- insta-clone/src/pages/Navigation/SuggestedUsers.jsx
+++ insta-clone/src/pages/Navigation/SuggestedUsers.jsx
@@ -1,7 +1,11 @@
 import {Box, Flex, Text, VStack, Link} from "@chakra-ui/react"
 import SuggestedHeader from "./SuggestedHeader"
 import SuggestedUser from "./SuggestedUser"
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers"
 const SuggestedUsers = () => {
+  const { isLoading, suggestedUsers} = useGetSuggestedUsers();
+
+  if(isLoading ) return null
   return (
     <VStack py={8} px={8} gap={4}>
         <SuggestedHeader/>
@@ -16,10 +20,11 @@ const SuggestedUsers = () => {
             See all
           </Text>
         </Flex>
-        <SuggestedUser name="Dick Smalls" followers={1392} avatar="https://bit.ly/dan-abramov"/>
-        <SuggestedUser name="Dick Big" followers={102} avatar="https://bit.ly/ryan-florence"/>
-        <SuggestedUser name="Richard Smalls" followers={372} avatar="https://bit.ly/code-beast"/>
-        <SuggestedUser name="Antonie Low" followers={92} avatar="https://bit.ly/dan-abramov"/>
+        
+        {suggestedUsers.map((user) => (
+          <SuggestedUser user={user} key={user.uid}/>
+        ))}
+
         <Box alignSelf={"start"} color={"gray.500"} mt={5}>
           © 2024 Built by <Link href="https://github.com/codemalcolm" _hover={{color:"gray.100"}} target="_blank">codemalcolm</Link>
 
